89a069ab30a43dcea046bf1599c0864c1fa7c0b1
hoanghohohaha <hoangng@cs.washington.edu>
2022-06-28 13:04:15 -0700

    set up staging on github

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { mainTheme } from './style/theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="CBR-fishModel">
       <ThemeProvider theme={mainTheme}>
         <App />
       </ThemeProvider>
